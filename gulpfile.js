55ebec2461b1bd6a158e4c83ae6ddc8a44d9a5ca
unknown <deepak52567@gmail.com>
2018-12-18 02:26:21 +0530

    Inital commit with top page style

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var autoprefix = require("gulp-autoprefixer"),
+    connect = require("gulp-connect"),
+    gulp = require("gulp"),
+    bourbon = require("bourbon").includePaths,
+    sass = require("gulp-sass");
+
+var paths = {
+    scss: ["public/sass/*.scss"]
+};
+
+gulp.task("sass", function () {
+    return gulp.src(paths.scss)
+        .pipe(sass({
+            sourcemaps: true,
+            includePaths: bourbon
+        }))
+        .pipe(autoprefix("last 2 versions"))
+        .pipe(gulp.dest("public/stylesheets"))
+        .pipe(connect.reload());
+});
+
+gulp.task('icons', function() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('public/webfonts/'));
+});
+
+gulp.task("connect", function () {
+    connect.server({
+        root: "public",
+        port: 8000,
+        livereload: true
+    });
+});
+
+gulp.task("default", ["sass", "connect", "icons"], function () {
+    gulp.watch(paths.scss, ["sass"]);
+});
